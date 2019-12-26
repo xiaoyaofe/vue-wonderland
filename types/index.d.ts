5e76d5712f00787d9c22d786f29109f9808997bf
zongjiang.ge <zongjiang.ge@xyroyale.com>
2019-12-26 12:29:59 +0800

    解决类型错误报错,和补签记录的初始化报错的问题

--- types/index.d.ts
+++ types/index.d.ts
@@ -209,7 +209,8 @@ export namespace OA {
         leaveType: number
         settingDate: string
         settingType: number
-        userId: string
+        userId: string,
+        remark:string
       }[]
       useDetailInfoList: UseDetailInfo[]
       produceDetailInfoList: ProduceDetailInfo[]
